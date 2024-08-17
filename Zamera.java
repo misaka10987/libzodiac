785ccba34b644eca501e3b0a789216365d788281
Blue-Flag-666 <64064866+Blue-Flag-666@users.noreply.github.com>
2024-08-18 02:27:22 +0800

    Update Zamera.java

--- Zamera.java
+++ Zamera.java
@@ -1,10 +1,17 @@
 package frc.libzodiac;
 
 import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cscore.UsbCamera;
 
 public class Zamera {
-    public Zamera()
-    {
-        CameraServer.startAutomaticCapture();
+    private UsbCamera camera;
+
+    public Zamera start() {
+        camera = CameraServer.startAutomaticCapture();
+        return this;
+    }
+
+    public void close() {
+        camera.close();
     }
 }
